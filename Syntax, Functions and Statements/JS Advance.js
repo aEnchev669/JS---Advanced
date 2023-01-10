f74c35478b761021c5d8f94be033a02fe1cd447a
aEnchev669 <aenchev669@abv.bg>
2023-01-10 19:51:14 +0200

    js

--- Syntax, Functions and Statements/JS Advance.js
+++ Syntax, Functions and Statements/JS Advance.js
@@ -1,4 +1,24 @@
 let a = 2;
 let b = 3;
 
+
 console.log(a + b);
+
+let name = 'Ivan';
+
+function start() {
+    let myVar = 5;
+
+    if (a == b)
+    {
+        let secVar = 10;
+    }
+}
+
+const qw = 'ivan';
+
+let qe = 'Ivan';
+
+qw = qe;
+
+console.log(qw);
