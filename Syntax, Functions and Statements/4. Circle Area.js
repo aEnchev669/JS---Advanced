cbcb0ddf6e68e6bc0ab5131c85174a4df86391f6
aEnchev669 <aenchev669@abv.bg>
2023-01-11 14:29:18 +0200

    Js

--- Syntax, Functions and Statements/4. Circle Area.js
+++ Syntax, Functions and Statements/4. Circle Area.js
@@ -1,13 +1,13 @@
 function area(num){
 let result;
-    if(typeof(num) == 'number')
+    if(typeof(num) === 'number')
     {
       result = Math.pow(num, 2) * Math.PI;
       console.log(result.toFixed(2));
     }
     else
     {
-     console.log('We can not calculate the circle area, because we receive a string.');
+     console.log(`We can not calculate the circle area, because we receive a ${typeof(num)}.`);
     }
 }
 
