eda19527a34d0cccb48e963ebe796e2cee6d7b58
aEnchev669 <aenchev669@abv.bg>
2023-01-11 18:57:41 +0200

    js

---
+++ Exercise Syntax, Functions and Statements/5. Time to Walk.js
@@ -0,0 +1,18 @@
+function walk(steps, footprint, speed)
+{
+    const totalMeters = steps * footprint;
+    const speedInMetersInSec = speed / 3.6;
+    const bonusMins = totalMeters % 500 / 100;
+    const time = totalMeters / speedInMetersInSec;
+
+   
+    const timeInH = Math.floor(time / 3600);
+    const timeInMin = Math.floor(time / 60);
+    const timeInSec = Math.floor(time % 60);
+    const totalMins = Math.round(timeInMin + bonusMins);
+    console.log(`${timeInH}:${totalMins}:${timeInSec}`);
+    
+
+}
+
+walk(9000, 0.60, 5);
