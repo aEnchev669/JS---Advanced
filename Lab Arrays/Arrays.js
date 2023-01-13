dcb18ff5926e764d406b9622fe610d83241ce91a
aEnchev669 <aenchev669@abv.bg>
2023-01-13 14:41:06 +0200

    Create Arrays.js

---
+++ Lab Arrays/Arrays.js
@@ -0,0 +1,15 @@
+let myArr = [10, 20, 30, 40, 50, 60];
+
+console.log(myArr.length - 1);
+console.log(myArr.length);
+
+myArr[1.3] = 'Ivan';
+myArr[1.4] = 'Gosho';
+myArr['gosho'] = 'goga';
+
+console.log(myArr[1.3]);
+console.log(myArr[1.4]);
+
+console.log(myArr[-2]);
+
+console.log(myArr['gosho']);
