4d8698ff15afd930c7947f41d2745a9df360f636
aEnchev669 <aenchev669@abv.bg>
2023-01-14 19:56:29 +0200

    Update 11. Equal Neighbors.js

--- Lab Arrays/11. Equal Neighbors.js
+++ Lab Arrays/11. Equal Neighbors.js
@@ -1,4 +1,4 @@
-function equalNeighbors(arr){
+function equal(arr){
 
     let sum = 0;
     for (let i = 0; i < arr.length - 1; i++) {
@@ -14,14 +14,14 @@ function equalNeighbors(arr){
     for (let i = 0; i < arr.length; i++) {
         for (let j = 0; j < arr[i].length - 1; j++) {
         
-         if(arr[i][j] == arr[i][j - 1])
+         if(arr[i][j] == arr[i][j + 1])
          {
              sum ++;
          }
         }
      }
 
-    console.log(sum);
+    return(sum);
 }
 
-equalNeighbors([[2, 2, 5, 7, 4], [4, 0, 5, 3, 4], [2, 5, 5, 4, 2]]);
+equal([[2, 2, 5, 7, 4], [4, 0, 5, 3, 4], [2, 5, 5, 4, 2]]);
