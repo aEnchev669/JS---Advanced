e2bbb17a4a60a30ef95664b886d0c32d485356c0
aEnchev669 <aenchev669@abv.bg>
2023-01-15 23:00:57 +0200

    Update 7. Sorting Numbers.js

--- Lab Arrays/7. Sorting Numbers.js
+++ Lab Arrays/7. Sorting Numbers.js
@@ -1,7 +1,12 @@
-function sort(arr){
-
-    arr.sort((a,b) => a-b);
-
-    console.log(arr);
+function sort(arr) {
+    let result = [];
+    arr.sort((a, b) => a - b);
+    
+for (let i = 0; i <= arr.length / 2 ; i++) {
+  
+    result.push(arr.shift());
+    result.push(arr.pop());
+}
+    console.log(result);
 }
 sort([1, 65, 3, 52, 48, 63, 31, -3, 18, 56]);
