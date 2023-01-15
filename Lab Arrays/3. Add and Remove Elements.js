7f23497f04502b2a64abfca7509f247bae7aebb1
aEnchev669 <aenchev669@abv.bg>
2023-01-15 22:16:14 +0200

    Create 3. Add and Remove Elements.js

---
+++ Lab Arrays/3. Add and Remove Elements.js
@@ -0,0 +1,19 @@
+function addNRemove(arr) {
+    let num = 1;
+    let result = [];
+
+    arr.forEach(element => {
+        if (element === 'add') {
+            result.push(num)
+        }
+        else if (element === 'remove') {
+            result.pop();
+        }
+        
+        num++;
+    });
+
+    console.log(  result.length != 0 ? result.join('\n') : 'Empty');
+}
+
+addNRemove(['remove', 'remove', 'remove']);
