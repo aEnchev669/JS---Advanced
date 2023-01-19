5f97a425d56f53aa567a95c3024459592417a1e7
aEnchev669 <aenchev669@abv.bg>
2023-01-19 23:47:26 +0200

    Create 1. City Record.js

---
+++ Objects & Composition/1. City Record.js
@@ -0,0 +1,14 @@
+function record(name, pop, treasury){
+    const cityRecord = {
+        'name' : name,
+        'population' : pop,
+        'treasury' : treasury
+    }
+    return(cityRecord);
+}
+
+console.log(record('Tortuga',
+
+7000,
+
+15000));
