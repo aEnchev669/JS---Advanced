e4629569ef07ef58fa77fb02b8ce39b2bb6fc94f
aEnchev669 <aenchev669@abv.bg>
2023-01-25 13:50:24 +0200

    Update SumNumbers.js

--- DOM Introduction/New folder/03.Sum Numbers/SumNumbers.js
+++ DOM Introduction/New folder/03.Sum Numbers/SumNumbers.js
@@ -3,8 +3,6 @@ function calc() {
     const num1 = document.getElementById('num1').value;
     const num2 = document.getElementById('num2').value;
     const sum = document.getElementById('sum');
-   
-    sum.value = Number(num1) + Number(num2);
 
-    
+    sum.value = Number(num1) + Number(num2);
 }
