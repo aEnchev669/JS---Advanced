651ab82fca2ea032de2c19316d8945b4bfb24b9a
aEnchev669 <aenchev669@abv.bg>
2023-01-26 15:52:50 +0200

    Js

---
+++ DOM Introduction/Lab/04.Show More/ShowMore.js
@@ -0,0 +1,9 @@
+function showText() {
+    const text = document.getElementById('text');
+
+    text.style.display = 'inline';
+
+    const more = document.getElementById('more');
+
+    more.style.display = 'none';
+}
