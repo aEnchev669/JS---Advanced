c00e92170ac20124556c8585a0185b7fcbcee0c6
aEnchev669 <aenchev669@abv.bg>
2023-01-26 15:53:05 +0200

    Js

--- DOM Introduction/New folder/04.Show More/ShowMore.js
+++
@@ -1,9 +0,0 @@
-function showText() {
-    const text = document.getElementById('text');
-
-    text.style.display = 'inline';
-
-    const more = document.getElementById('more');
-
-    more.style.display = 'none';
-}
