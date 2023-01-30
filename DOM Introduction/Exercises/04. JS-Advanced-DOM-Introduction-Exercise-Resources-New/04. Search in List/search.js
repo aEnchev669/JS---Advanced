88e099373574f2c1d7170aabf699ac1c57df7d01
aEnchev669 <aenchev669@abv.bg>
2023-01-30 21:42:18 +0200

    Update search.js

--- DOM Introduction/Exercises/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/04. Search in List/search.js
+++ DOM Introduction/Exercises/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/04. Search in List/search.js
@@ -1,3 +1,22 @@
 function search() {
-   // TODO
+   const list = Array.from(document.querySelectorAll('#towns li'));
+   const result = document.getElementById('result');
+   const search = document.getElementById('searchText');
+
+   let counter = 0;
+
+   list.forEach(element => {
+
+      if (element.textContent.includes(search.value)) {
+         counter++;
+         element.style.fontWeight = 'bold';
+         element.style.textDecoration = 'underline';
+      }
+      else{
+         element.style.fontWeight = 'normal';
+         element.style.textDecoration = 'none';
+      }
+   });
+
+   result.textContent = `${counter} matches found`;
 }
