03ffbc21bcaef51dd19a96b423d663a79e353003
aEnchev669 <aenchev669@abv.bg>
2023-01-30 20:12:19 +0200

    Update solution.js

--- DOM Introduction/Exercises/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/02. Pascal or Camel Case/solution.js
+++ DOM Introduction/Exercises/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/02. Pascal or Camel Case/solution.js
@@ -1,32 +1,59 @@
+// function solve() {
+
+//   const input = document.getElementById('text').value;
+//   const currCase = document.getElementById('naming-convention').value;
+//   const result = document.getElementById('result');
+//   let output = '';
+
+//   let arr = input.split(" ");
+//   if (currCase == 'Pascal Case') {
+//     for (const word of arr) {
+//       let lowerCase = word.toLowerCase();
+//       output += lowerCase.charAt(0).toUpperCase() + lowerCase.slice(1);
+//     }
+//   }
+//   else if (currCase == 'Camel Case') {
+//     for (let i = 0; i < arr.length; i++) {
+//       let lowerCase = arr[i].toLowerCase();
+//       if (i == 0) {
+//         output += lowerCase;
+//       }
+//       else {
+//         output += lowerCase.charAt(0).toUpperCase() + lowerCase.slice(1);
+//       }
+//     }
+//   }
+//   else {
+//     output = 'Error!';
+//   }
+//   result.textContent = output.toString();
+// }
 function solve() {
 
   const input = document.getElementById('text').value;
   const currCase = document.getElementById('naming-convention').value;
   const result = document.getElementById('result');
-  let output = '';
 
-  let arr = input.split(" ");
-  if (currCase == 'Pascal Case') {
-    for (const word of arr) {
-      let lowerCase = word.toLowerCase();
-      output += lowerCase.charAt(0).toUpperCase() + lowerCase.slice(1);
-    }
+  const toPascal = (text) => {
+    text
+      .split(" ")
+      .map(i => i[0].toUpperCase() + i.slice(1).toLowerCase())
+      .join("");
   }
-  else if (currCase == 'Camel Case') {
-    for (let i = 0; i < arr.length; i++) {
-      let lowerCase = arr[i].toLowerCase();
-      if (i == 0) {
-        output += lowerCase;
-      }
-      else {
-        output += lowerCase.charAt(0).toUpperCase() + lowerCase.slice(1);
-      }
-    }
+
+  const toCamel = (text) => {
+
+    text = toPascal(text);
+    return text[0].toLowerCase() + text.slice(1);
+  }
+
+  if (currCase == "Pascal Case") {
+    result.textContent = toPascal(input);
+  }
+  else if (currCase == "Camel Case") {
+    result.textContent = toCamel(input);
   }
   else {
-    output = 'Error!';
+    result.textContent = 'Error!';
   }
-  result.textContent = output.toString();
 }
-
-solve("Invalid Input", "Another Case");
