abaf462d2100e80f39d84bf89e6ce48d89bfd56a
aEnchev669 <aenchev669@abv.bg>
2023-01-30 19:57:14 +0200

    Update subtract.js

--- DOM Introduction/Exercises/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/01. Subtraction/subtract.js
+++ DOM Introduction/Exercises/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/01. Subtraction/subtract.js
@@ -1,3 +1,8 @@
 function subtract() {
-    console.log('TODO:...');
+    const firstNum = Number(document.getElementById('firstNumber').value);
+    const secondNum = Number(document.getElementById('secondNumber').value);
+
+    let result = document.getElementById('result');
+    result.textContent = firstNum - secondNum;
+    console.log();
 }
