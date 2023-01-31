6d6554512b0c1ddff324dd78d12767f344a9775b
aEnchev669 <aenchev669@abv.bg>
2023-01-31 18:34:59 +0200

    Update app.js

--- DOM Introduction/Exercises/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/07. Hell's Kitchen/app.js
+++ DOM Introduction/Exercises/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/07. Hell's Kitchen/app.js
@@ -1,8 +1,9 @@
 function solve() {
    document.querySelector('#btnSend').addEventListener('click', onClick);
+const input = document.querySelector('#inputs>textarea').value;
 
    function onClick () {
       //   TODO:
-      
+      console.log(input);
    }
 }
