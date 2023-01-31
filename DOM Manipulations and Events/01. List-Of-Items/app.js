361ef35bf37424a82477803dc13f4a43d148e276
aEnchev669 <aenchev669@abv.bg>
2023-02-01 01:55:49 +0200

    Update app.js

--- DOM Manipulations and Events/01. List-Of-Items/app.js
+++ DOM Manipulations and Events/01. List-Of-Items/app.js
@@ -1,3 +1,11 @@
 function addItem() {
-    console.log('TODO:...');
+    const input = document.getElementById('newItemText');
+    const newLi = document.createElement('li');
+    newLi.textContent = input.value;
+
+    const list = document.getElementById('items');
+
+    list.appendChild(newLi);
+
+    input.value = '';
 }
