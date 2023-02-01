9825213c59e57f7d1faa6e9e1dc584619cf6f81d
aEnchev669 <aenchev669@abv.bg>
2023-02-01 02:31:47 +0200

    Update app.js

--- DOM Manipulations and Events/02. Delete-From-Table/app.js
+++ DOM Manipulations and Events/02. Delete-From-Table/app.js
@@ -1,3 +1,18 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    const input = document.querySelector('input[name="email"]').value;
+    const customers = document.querySelectorAll('#customers tbody tr');
+
+    const arrOfCustomers = Array.from(customers);
+
+    let deleted = false;
+    for (let row of arrOfCustomers) {
+        if (row.children[1].textContent == input) {
+            row.remove();
+            deleted = true;
+        }
+    }
+    document.getElementById('result').textContent = deleted ? 'Deleted' : 'Not found';
+    
+       
+    
 }
