0427024b3537bb30e5b421095edf41fb3214e3c0
aEnchev669 <aenchev669@abv.bg>
2023-02-01 16:51:44 +0200

    Update app.js

--- DOM Manipulations and Events/04. Mouse-Gradient/app.js
+++ DOM Manipulations and Events/04. Mouse-Gradient/app.js
@@ -1,3 +1,11 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+    const gradient = document.getElementById('gradient');
+    gradient.addEventListener('mousemove', onClick);
+
+    function onClick(event){
+        const percent = (Math.floor(event.offsetX / 3));
+
+        document.getElementById('result').textContent = percent + '%';
+    }
+
 }
