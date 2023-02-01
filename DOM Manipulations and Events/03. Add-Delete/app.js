13358d920472e0dcda7cd4c9fbd847a0530e30b9
aEnchev669 <aenchev669@abv.bg>
2023-02-01 16:51:43 +0200

    Update app.js

--- DOM Manipulations and Events/03. Add-Delete/app.js
+++ DOM Manipulations and Events/03. Add-Delete/app.js
@@ -1,3 +1,17 @@
 function addItem() {
-    //TODO...
+    const input = document.getElementById('newItemText');
+    const newLi = document.createElement('li');
+    newLi.textContent = input.value;
+
+    const list = document.getElementById('items');
+
+    const deleteBtn = document.createElement('a');
+    deleteBtn.href = '#';
+    deleteBtn.textContent = '[Delete]';
+
+    deleteBtn.addEventListener('click', () => newLi.remove());
+    newLi.appendChild(deleteBtn);
+    list.appendChild(newLi);
+
+    input.value = '';
 }
