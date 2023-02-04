bbd8938f45a2f2ddb8b88db62f3ff91d060ed01b
aEnchev669 <aenchev669@abv.bg>
2023-02-04 03:07:04 +0200

    Update app.js

--- DOM Manipulations and Events/05. Encode-and-Decode-Messages/app.js
+++ DOM Manipulations and Events/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,38 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    const textArea = document.querySelectorAll('textarea');
+    const allButtons = document.querySelectorAll('button');
+    
+    
+
+    allButtons[0].addEventListener('click', encode);
+    allButtons[1].addEventListener('click', decode);
+
+    function encode() {
+        let text = '';
+        let input = textArea[0].value;
+
+        for (let i = 0; i < input.length; i++) {
+
+            const element = input[i];
+            text += String.fromCharCode(element.charCodeAt(0) + 1);
+        }
+
+        textArea[1].value = text;
+        textArea[0].value = '';
+    }
+
+    function decode() {
+        let text = '';
+        const input = textArea[1].value;
+        for (let i = 0; i < input.length; i++) {
+
+            const element = input[i];
+            text += String.fromCharCode(element.charCodeAt(0) - 1);
+        }
+
+        textArea[1].value = text;
+        textArea[0].value = '';
+    }
+   
+
 }
