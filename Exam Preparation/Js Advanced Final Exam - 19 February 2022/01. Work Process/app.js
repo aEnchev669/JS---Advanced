c14c8cc56c40a1bc80803ecc1da4ba30de61ab50
aEnchev669 <aenchev669@abv.bg>
2023-02-14 15:22:09 +0200

    Update app.js

--- Exam Preparation/Js Advanced Final Exam - 19 February 2022/01. Work Process/app.js
+++ Exam Preparation/Js Advanced Final Exam - 19 February 2022/01. Work Process/app.js
@@ -1,4 +1,20 @@
 function solve() {
-    //TODO
+    const inputFName = document.getElementById('fname');
+    const inputName = document.getElementById('lname');
+    const inputEmail = document.getElementById('email');
+    const inputBirth = document.getElementById('birth');
+    const inputPosition = document.getElementById('position');
+    const inputSalary = document.getElementById('salary');
+   //    const salarySum = document.getElementById('sum');
+
+    console.log(inputFName);
+    console.log(inputName);
+    console.log(inputEmail.value);
+    console.log(inputBirth.value);
+    console.log(inputPosition.value);
+    console.log(inputSalary.value);
+    // console.log(salarySum.value);
+
+    
 }
 solve()
