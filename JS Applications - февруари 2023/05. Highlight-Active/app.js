acba02c8a8b22a48122a44d6e01a430a70eb1fab
aEnchev669 <aenchev669@abv.bg>
2023-03-09 21:18:25 +0200

    JS

---
+++ JS Applications - февруари 2023/05. Highlight-Active/app.js
@@ -0,0 +1,13 @@
+function focused() {
+    const elements = Array.from(document.querySelectorAll('input'));
+
+    for (const element of elements) {
+        element.addEventListener('focus', () =>{
+            element.parentElement.className = 'focused';
+        });
+
+        element.addEventListener('blur', () =>{
+            element.parentElement.className = '';
+        });
+    }
+}
